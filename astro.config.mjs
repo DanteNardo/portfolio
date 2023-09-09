05d63333ca237f9f1ebbb5c82765b178c8f5ff59
DanteNardo <nardogamedev@outlook.com>
2023-09-08 23:57:31 -0700

    Change icons, improve responsiveness

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    applyBaseStyles: false
-  }), react()]
-});
+  integrations: [
+    mdx(),
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
+})
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,20 +1,18 @@
-import Navigation from "@/components/navigation/Navigation";
-import ResumeButton from "@/components/resume/ResumeButton";
+import Navigation from '@/components/navigation/Navigation'
+import ResumeButton from '@/components/resume/ResumeButton'
 
 export default function Header() {
   return (
-    <header
-        className="print:hidden relative z-50 flex w-full gap-3 px-6 py-3 items-center justify-between border-b border-base-700 bg-base-950"
-      >
-        <div className="flex gap-6 items-center">
-          <a href="/" className="flex flex-col gap-1 rounded acc-focus">
-            <h2 className="text-2xl font-bold tracking-tight text-focus">
-              Dante Nardo
-            </h2>
-          </a>
-          <Navigation />
-        </div>
-        <ResumeButton />
-      </header>
+    <header className="print:hidden relative z-50 flex flex-wrap w-full gap-3 px-6 py-3 items-center justify-between border-b border-base-700 bg-base-950">
+      <a href="/" className="flex flex-col gap-1 mr-6 rounded acc-focus">
+        <h2 className="text-2xl font-bold tracking-tight text-focus whitespace-nowrap">
+          Dante Nardo
+        </h2>
+      </a>
+      <span className="flex-grow order-last sm:order-none overflow-x-auto">
+        <Navigation />
+      </span>
+      <ResumeButton />
+    </header>
   )
 }
