560013d80737a0053795b8261064f655ded45486
DanteNardo <nardogamedev@outlook.com>
2023-09-10 14:17:03 -0700

    Added PDF download

--- src/components/resume/ResumeButton.tsx
+++ src/components/resume/ResumeButton.tsx
@@ -1,12 +1,24 @@
-import { FileText } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { FileText, Download } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 export default function ResumeButton() {
   return (
-    <Button variant="default" asChild>
-      <a href="/resume">
-        <FileText className="mr-2 h-4 w-4" /> Resume
+    <div className="flex gap-0.5 rounded bg-black">
+      <a
+        href="/resume"
+        className="flex gap-2 px-3 py-1 items-center rounded-l bg-white text-black"
+      >
+        <FileText className="w-4 h-4" />
+        <span className="text-base">Resume</span>
       </a>
-    </Button>
+      <a
+        href="/resume-dante-nardo.pdf"
+        download
+        title="Download resume"
+        className="flex items-center justify-center rounded-r pl-2 pr-2 bg-white text-black"
+      >
+        <Download className="w-4 h-4" />
+      </a>
+    </div>
   );
 }
